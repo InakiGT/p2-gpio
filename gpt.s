f762e9b8d930e8b7bb70408cbba8b2fad5abf2d9
Iñaki García <inakigarciat@gmail.com>
2023-03-04 00:21:59 -0600

    Muchas cosas

--- gpt.s
+++ gpt.s
@@ -44,63 +44,67 @@ setup:
 
         # set led status initial value
         ldr     r0, =GPIOC_ODR @ moves address of GPIOC_ODR register to r0
-        ldr     r7, =GPIOB_ODR
+        mov	r4, #0x0
+	str	r4, [r0]
+	ldr     r7, =GPIOB_ODR
+	mov	r4, #0xFFFF
+	str	r7, [r4] 
         ldr     r8, =GPIOA_IDR
-        mov     r1, 0x0
-
+reset_count:
+	mov	r2, #0x0
 loop:
-    @; Comprueba si el push button A0 está siendo presionado
-    ldr r0, =GPIOA_IDR
-    ldr r1, [r0]
-    and r1, r1, #0x01
-    cmp r1, #0
-    beq inc_count     @; Si el push button A0 está siendo presionado, salta a la etiqueta "inc_count"
+    	@; Comprueba si el push button A0 está siendo presionado
+    	ldr r0, =GPIOA_IDR
+    	ldr r1, [r0]
+    	and r1, r1, #0x01
+    	cmp r1, 0x0
+    	beq inc_count     @; Si el push button A0 está siendo presionado, salta a la etiqueta "inc_count"
     
-    @; Comprueba si el push button A4 está siendo presionado
-    ldr r0, =GPIOA_IDR
-    ldr r1, [r0]
-    and r1, r1, #0x10
-    cmp r1, #0
-    beq dec_count     @; Si el push button A4 está siendo presionado, salta a la etiqueta "dec_count"
+    	@; Comprueba si el push button A4 está siendo presionado
+    	ldr r0, =GPIOA_IDR
+    	ldr r1, [r0]
+    	and r1, r1, #0x10
+    	cmp r1, #0
+    	beq dec_count     @; Si el push button A4 está siendo presionado, salta a la etiqueta "dec_count"
 
-    @; Si ninguno de los botones está siendo presionado, sigue leyendo el valor de los botones
-    b loop
+    	@; Si ninguno de los botones está siendo presionado, sigue leyendo el valor de los botones
+	b loop
 
 inc_count:
-    @; Incrementa el contador
-    add r2, r2, #1
-    cmp r2, #10
-    bgt reset_count   @; Si el contador supera el valor 10, salta a la etiqueta "reset_count"
+    	@; Incrementa el contador
+    	add r2, r2, #1
+    	cmp r2, #10
+    	bgt reset_count   @; Si el contador supera el valor 10, salta a la etiqueta "reset_count"
     
-    @; Enciende los LEDs correspondientes al contador actual
-    ldr r0, =GPIOB_BSRR
-    mov r1, r2
-    lsl r1, r1, #16
-    str r1, [r0]     @; Escribe el valor del contador en los bits 0-9 del registro GPIOB_BSRR para encender los LEDs correspondientes
-    b loop
+    	@; Enciende los LEDs correspondientes al contador actual
+    	ldr r0, =GPIOB_ODR
+    	mov r1, r2
+    	lsl r1, r1, #16
+    	str r1, [r0]     @; Escribe el valor del contador en los bits 0-9 del registro GPIOB_BSRR para encender los LEDs correspondientes
+    	b loop
 
 dec_count:
-    @; Decrementa el contador
-    sub r2, r2, #1
-    cmp r2, #-1
-    blt reset_count   @; Si el contador es menor que 0, salta a la etiqueta "reset_count"
+   	 @; Decrementa el contador
+    	sub r2, r2, #1
+    	cmp r2, #-1
+    	blt reset_count   @; Si el contador es menor que 0, salta a la etiqueta "reset_count"
 
     @@; Enciende los LEDs correspondientes al contador actual
-    ldr r0, =GPIOB_BSRR
-    mov r1, r2
-    lsl r1, r1, #16
+    	ldr r0, =GPIOB_ODR
+    	mov r1, r2
+    	lsl r1, r1, #16
 
-@; Escribe el valor del contador en los bits 0-9 del registro GPIOB_BSRR para encender los LEDs correspondientes
-mov r3, #0x03FF
-and r1, r1, r3
-lsl r1, r1, #16
-str r1, [r0]
-b loop
+	@; Escribe el valor del contador en los bits 0-9 del registro GPIOB_BSRR para encender los LEDs correspondientes
+	mov r3, #0x03FF
+	and r1, r1, r3
+	lsl r1, r1, #16
+	str r1, [r0]
+	b loop
 
-@; Apaga todos los LEDs
-ldr r0, =GPIOB_BSRR
-mov r1, #0x0000
-str r1, [r0]
-b loop
+	@; Apaga todos los LEDs
+	ldr r0, =GPIOB_ODR
+	mov r1, #0x0000
+	str r1, [r0]
+	b loop
 
 
